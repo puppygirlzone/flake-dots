4a58d2dafe073fd10b41519f7474485ef6bdd9f0
ItsNotAhlo <153831898+ItsNotAhlo@users.noreply.github.com>
2024-08-20 00:49:58 +0930

    Had to add git. Don't wanna talk about it lmao.

--- configuration.nix
+++ configuration.nix
@@ -134,8 +134,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
